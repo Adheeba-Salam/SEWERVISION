2a5dc45b2f5d4b836cf1723583a0eedef69acf79
Adheeba Salam <adheeba00@gmail.com>
2026-08-18 16:19:49 +0530

    Delete train_test.py

--- train_test.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# Load the model
-model = YOLO('yolo11s.pt') 
-
-# Start training - this will take time!
-model.train(data='pipe.yaml', epochs=100, imgsz=640, batch=8,device='Gpu')
-
-print("Step 4: Training finished", flush=True)
